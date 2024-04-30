dc30082baaff1639c47f62958d17129c7b1362d1
erGoProcsee <ernesto.gomez.338@my.csun.edu>
2024-04-29 21:17:04 -0700

    landing page

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -3,6 +3,9 @@
 import Link from "next/link"
 import React, { useEffect, useState } from "react";
 
+import Image from 'next/image';
+import NavLink from "./navLink";
+
 const links = [
     { url: "/", title: "Home" },
     { url: "/about", title: "About" },
@@ -15,15 +18,34 @@ const Navbar = () => {
     const [open, setOpen] = useState(false);
 
     return (
-        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        // menu links
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+            <div className="hidden md:flex gap-4">
+                {links.map(link => (
+                    <NavLink link={link} key={link.title} />
+                ))}
+            </div>
 
             {/* logo */}
-            <div className="md:hidden">
+            <div className="md:hidden lg:flex">
                 <Link
                     href="/"
-                    className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
+                    className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+                >
                     <span className="text-white mr-1">erin</span>
-                    <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">go</span>
+                    <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
+                        go
+                    </span>
+                </Link>
+            </div>
+
+            {/* social links */}
+            <div className="flex gap-4">
+                <Link href="https://github.com/erGoProcsee">
+                    <Image src="/github.png" alt="github icon" width={24} height={24} />
+                </Link>
+                <Link href="https://github.com/erGoProcsee">
+                    <Image src="/linkedin.png" alt="linkedin icon" width={24} height={24} />
                 </Link>
             </div>
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello Woorld</div>;
+  return <div className="h-full flex flex-col">
+    {/* img container */}
+    <div className="h-1/2 relative">
+      <Image src="/hero.png" alt="" fill className="object-contain" />
+    </div>
+    {/* txt container */}
+    <div className="h-1/2 flex flex-col gap-8 items-center justify-center">
+      {/* title */}
+      <h1 className="text-4xl font-bold">
+        Craftin
+      </h1>
+      {/* description */}
+      <p className="">
+        cogito ergo sum
+      </p>
+    </div>
+  </div>;
 };
 
 export default Homepage;
